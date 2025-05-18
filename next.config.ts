0425fec10ab27f2ee87eda41f8e1b9f86269492f
Maurice Filiatreault-Hebert <inbox@mauricefh.com>
2025-05-18 12:08:42 -0600

    update config file to fix path issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+      "@/lib": path.resolve(__dirname, "src/lib"),
+      "@/components": path.resolve(__dirname, "src/components"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
