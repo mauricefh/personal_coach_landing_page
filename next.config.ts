fee5b177e6e4b649a1bb70d9e3cde2dc8cbcbfc8
Maurice Filiatreault-Hebert <inbox@mauricefh.com>
2025-05-20 10:16:34 -0600

    fifth attempt to fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 import path from "path";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"),
-      "@/lib": path.resolve(__dirname, "src/lib"),
-      "@/components": path.resolve(__dirname, "src/components"),
-    };
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
